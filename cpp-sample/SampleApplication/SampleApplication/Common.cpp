70cca854606329c61af5734729f3df14cfe9443a
ctime-zliang <1156282805@qq.com>
2024-09-25 19:48:33 +0800

    feat: modify - daily commit

--- cpp-sample/SampleApplication/SampleApplication/Common.cpp
+++ cpp-sample/SampleApplication/SampleApplication/Common.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 
+#define PROHECT_NAME "Sample Application"
+
 const double PI = 22.0 / 7;
 
-double getArea(float radius)
+constexpr double PI_SQ()
+{
+    return PI * PI;
+}
+
+double getCirlceArea(float radius)
 {
     return PI * radius * radius;
+}
+
+float getCircumference(float radius)
+{
+    return PI * 2 * radius;
 }
 
 int getSum(int a, int b)
--- cpp-sample/SampleApplication/SampleApplication/SampleApplication.cpp
+++ cpp-sample/SampleApplication/SampleApplication/SampleApplication.cpp
@@ -1,4 +1,6 @@
 ﻿#include <iostream>
+#include <vector>
+#include <string>
 #include "./Common.cpp"
 
 /*
@@ -24,14 +26,48 @@ void showInputSumProgram()
 void showAreaProgram()
 {
     const float radius = 5.0;
-    double areaValue = getArea(radius);
-    std::cout << "The area result is: " << areaValue << "\n" << std::endl;
+    double areaValue = getCirlceArea(radius);
+    float circumfValue = getCircumference(radius);
+    std::cout << "The area result is: " << areaValue << std::endl;
+    std::cout << "The circumference result is: " << circumfValue << "\n" << std::endl;
+}
+
+void showFixedArray()
+{
+    int arr1[5] = { 0 };
+    std::cout << "The array is: " << arr1 << "\n" << std::endl;
+}
+
+void showDynamicArray()
+{
+    // 创建动态数组
+    std::vector<int> dynArray(0);
+    std::cout << "The dynamic array size of dynArray is: " << dynArray.size() << "\n" << std::endl;
+    int inputValue = 0;
+    std::cout << "Please input a number:" << std::endl;
+    std::cin >> inputValue;
+    dynArray.push_back(inputValue);
+    std::cout << "The dynamic array size of dynArray is: " << dynArray.size() << "\n" << std::endl;
+    std::cout << "Please input a number:" << std::endl;
+    std::cin >> inputValue;
+    dynArray.push_back(inputValue);
+    std::cout << "The dynamic array size of dynArray is: " << dynArray.size() << "\n" << std::endl;
+}
+
+void showCPPSTDString()
+{
+    // 创建标准 C++ 字符串
+    std::string title ("Hello World");
+    std::cout << title << "\n" << std::endl;
 }
 
 int main()
 {
     showAreaProgram();
-    showInputSumProgram();    
+    // showInputSumProgram();  
+    showFixedArray();
+    // showDynamicArray();
+    showCPPSTDString();
     return 0;
 }
 
